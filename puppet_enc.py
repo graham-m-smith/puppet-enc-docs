4de4a18f2f35a0b0ac9985c39e8dbafdfbcdf3d3
Graham Smith <gsmith@gmsnet.co.uk>
2022-02-23 06:46:48 +0000

    Update

--- puppet_enc.py
+++ puppet_enc.py
@@ -2,34 +2,36 @@
 from diagrams import Diagram, Cluster
 from diagrams.azure.database import DatabaseForMysqlServers, DatabaseForPostgresqlServers
 from diagrams.azure.compute import FunctionApps, AppServices, VM
+from diagrams.azure.security import KeyVaults
 from diagrams.programming.language import Python
 
 with Diagram("Puppet ENC", show=False):
 
     with Cluster('Azure UK South'):
-        mysqldb = DatabaseForMysqlServers('puppetencdb')
-        apifuncapp = FunctionApps('puppetencapi')
-        uiappservice = AppServices('puppetencui')
+        mysqldb = DatabaseForMysqlServers('MySQL DB\rpuppetencdb')
+        apifuncapp = FunctionApps('Function App\rpuppetencapi')
+        uiappservice = AppServices('App Service\rpuppetencui')
+        keyvault = KeyVaults('Key Vault\rkv-puppetenc')
 
         mysqldb << [apifuncapp, uiappservice]
+        apifuncapp << keyvault
 
     with Cluster('On-Prem VMware'):
         
         with Cluster('Puppet Server'):
             puppetserver = VM('puppetserver')
-            encprocess = Python('ENC\rscript')
-            syncprocess = Python('SYNC\rcron job')
-            cli = Python('CLI\rscript')
-            dyngroups = Python('DYNGROUPS\rcron job')
-            puppetdb = DatabaseForPostgresqlServers('puppetdb')
+            encprocess = Python('Script\rENC')
+            syncprocess = Python('Cron Job\rPuppet DB Sync')
+            cli = Python('Script\rCLI\r')
+            dyngroups = Python('Cron Job\rDYNGROUPS')
+            puppetdb = DatabaseForPostgresqlServers('PostGRES\rpuppetdb')
 
-            puppetserver - encprocess << mysqldb
             puppetserver - puppetdb >> syncprocess >> mysqldb
+            puppetserver - encprocess << mysqldb
             puppetserver - dyngroups >> mysqldb
             puppetserver - cli >> apifuncapp
-
+ 
         with Cluster('Puppet Clients'):
-            puppetclient1 = VM('puppetclient1')
-            puppetclient2 = VM('puppetclient2')
+            puppetclient1 = VM('Puppet Agent')
 
-            encprocess >> [ puppetclient1, puppetclient2 ]
+            encprocess >> [ puppetclient1 ]

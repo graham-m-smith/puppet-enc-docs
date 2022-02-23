9804d06d5dd00756849f44f21cdd5bf6b97a26b8
Graham Smith <gsmith@gmsnet.co.uk>
2022-02-23 06:01:51 +0000

    Initial commit

---
+++ puppet_enc.py
@@ -0,0 +1,33 @@
+
+from diagrams import Diagram, Cluster
+from diagrams.azure.database import DatabaseForMysqlServers, DatabaseForPostgresqlServers
+from diagrams.azure.compute import FunctionApps, AppServices, VM
+from diagrams.programming.language import Python
+
+with Diagram("Puppet ENC", show=False):
+
+    with Cluster('Azure UK South'):
+        mysqldb = DatabaseForMysqlServers('puppetencdb')
+        apifuncapp = FunctionApps('puppetencapi')
+        uiappservice = AppServices('puppetencui')
+
+        mysqldb - [apifuncapp, uiappservice]
+
+    with Cluster('On-Prem VMware'):
+        
+        with Cluster('Puppet Server'):
+            puppetserver = VM('puppetserver')
+            encprocess = Python('ENC')
+            syncprocess = Python('SYNC')
+            cli = Python('CLI')
+            puppetdb = DatabaseForPostgresqlServers('puppetdb')
+
+            puppetserver - encprocess - mysqldb
+            puppetserver - puppetdb >> syncprocess >> mysqldb
+            puppetserver - cli - apifuncapp
+
+        with Cluster('Puppet Clients'):
+            puppetclient1 = VM('puppetclient1')
+            puppetclient2 = VM('puppetclient2')
+
+            encprocess >> [ puppetclient1, puppetclient2 ]

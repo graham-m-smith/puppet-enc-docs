f1084d466c0f554db8d81f258fb88dd78a627441
Graham Smith <gsmith@gmsnet.co.uk>
2022-03-04 04:38:42 +0000

    Update

---
+++ enc-process/enc-process.py
@@ -0,0 +1,35 @@
+from enum import auto
+from diagrams import Diagram, Cluster, Edge
+from diagrams.azure.database import DatabaseForMysqlServers, DatabaseForPostgresqlServers
+from diagrams.azure.compute import FunctionApps, AppServices, VM, ContainerRegistries, ContainerInstances
+from diagrams.azure.security import KeyVaults
+from diagrams.azure.storage import BlobStorage
+from diagrams.programming.language import Python
+from diagrams.custom import Custom
+
+with Diagram("ENC Process", show=False):
+
+    with Cluster('Azure UKS'):
+
+        keyvault = KeyVaults('Key Vault')
+        automation_account = Custom('automation account', '../resources/Automation-Accounts.png')
+        runbook = Custom('runbook', '../resources/runbook.png')
+        container_reg = ContainerRegistries('container reg')
+        config_gen_container = ContainerInstances('config gen container')
+        mysqldb = DatabaseForMysqlServers('MySQL DB')
+        blobstorage = BlobStorage('Blob Storage')
+
+    with Cluster('Puppet Server'):
+        encprocess = Python('Script\rENC')
+
+    with Cluster('Puppet Clients'):
+        puppetclient1 = VM('Puppet Agent')
+
+
+    automation_account >> runbook
+    keyvault >> Edge(label='Retrive Secrets', style='bold') >> runbook
+    container_reg >> Edge(label='Pull Image') >> config_gen_container
+    runbook >> Edge(label='Create CG & Container') >> config_gen_container
+    mysqldb >> \
+        Edge(label='Puppet\rConfig') >> \
+        config_gen_container >> blobstorage >> encprocess >> puppetclient1
